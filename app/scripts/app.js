90fd7e960eb8a36e3cb4db81dc3c82162ad94dc7
eugenebelov <eugene.beloff@gmail.com>
2015-11-05 18:59:54 +0200

    only 3 points could be

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,13 +2,19 @@
 	var MainScene = {
 		canvas: document.getElementById('mainScene'),
 		canvasContext: document.getElementById('mainScene').getContext('2d'),
+		counter: 3,
 
 		init: function() {
 				this.canvas.addEventListener('click', this.putPointToScene.bind(this), false);
 		},
 
 		putPointToScene: function(event) {
-			this.drawPoint(event.layerX, event.layerY)
+			if( this.counter > 0) {
+				this.counter--
+				this.drawPoint(event.layerX, event.layerY)
+			} else {
+				console.warn("Oops, 3 points already here!");
+			}
 		},
 
 		drawPoint: function(_x, _y) {
