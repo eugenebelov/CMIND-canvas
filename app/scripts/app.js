2c6555c8b718aa21826a83a3c4c27c64664edb72
eugenebelov <eugene.beloff@gmail.com>
2015-11-05 18:57:43 +0200

    add circles on canvas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,23 @@
 (function() {
-	console.log("main module");
+	var MainScene = {
+		canvas: document.getElementById('mainScene'),
+		canvasContext: document.getElementById('mainScene').getContext('2d'),
+
+		init: function() {
+				this.canvas.addEventListener('click', this.putPointToScene.bind(this), false);
+		},
+
+		putPointToScene: function(event) {
+			this.drawPoint(event.layerX, event.layerY)
+		},
+
+		drawPoint: function(_x, _y) {
+			this.canvasContext.beginPath();
+			this.canvasContext.arc(_x, _y, 11, 0, 2*Math.PI);
+			this.canvasContext.stroke();
+		},
+
+	};
+
+	MainScene.init();
 })();
