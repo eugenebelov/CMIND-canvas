0839bc5bfcbed8f9de4af82aad6929616e045ad9
Eugene <eugene.beloff@gmail.com>
2015-11-07 23:00:42 +0200

    reset svg

--- app/scripts/appd3.js
+++ app/scripts/appd3.js
@@ -1,6 +1,7 @@
 (function() {
 	var MainScene = {
 		canvas: d3.select('#mainCanvas'),
+		resetButton: d3.select('#resetButton'),
 		canvasNode: d3.select('#mainCanvas')[0][0],
 		drag: d3.behavior.drag(),
 		counter: 3,
@@ -8,26 +9,24 @@
 
 		init: function() {
 			console.log("init");
-			this.clickHandler = this.putPointToScene.bind(this);
+			this.setVerticleHandler = this.putPointToScene.bind(this);
+			this.setResetHandler = this.resetSvgCanvas.bind(this);
 
-			this.canvas.on('click', this.clickHandler);
-
+			this.canvas.on('click', this.setVerticleHandler);
+			this.resetButton.on('click', this.setResetHandler);
 		},
 
 		putPointToScene: function(event) {
 			if(d3.event.target == this.canvasNode) {
-				// this.drawPoint(d3.event.offsetX, d3.event.offsetY);
 				if( this.counter > 0) {
 					this.counter--;
 					this.points.push({ x: d3.event.offsetX, y: d3.event.offsetY });
 
-				// 	this.drawPoint(event.layerX, event.layerY);
+					this.drawPoint(d3.event.offsetX, d3.event.offsetY);
 				}
 				else {
-						// this.canvas.off('click', this.clickHandler);
-						this.drawPoly()
-						// console.log(this.points);
-
+					this.canvas.on('click', null);
+					this.drawPoly();
 				}
 			}
 		},
@@ -35,7 +34,6 @@
 		drawPoint: function(_x, _y) {
 			this.canvas
 					.append("circle")
-					.attr("point", this.counter)
 					.attr("cx", _x)
 					.attr("cy", _y)
 					.attr("r", 11);
@@ -45,23 +43,30 @@
 			var coords = this.convertCoordsToPoly(this.points);
 			console.log('lolol', coords);
 
-			// this.canvas
-			// 		.append("polygon")
-			// 		.attr('stroke', 'blue')
-			// 		.attr('stroke-width', '2')
-			// 		.attr("points", points);
+			this.canvas
+					.append("polygon")
+					.attr('stroke', 'blue')
+					.attr('stroke-width', '2')
+					.attr("points", coords);
 		},
 
 		convertCoordsToPoly: function(coords) {
 			var result = "";
 
-			result = _.map(coords, function(item) {
+			_.each(coords, function(item) {
 				var val = _.values(item);
-				return result += (val + " ");
+				result += (val + " ");
 			});
-			console.log(result);
 
 			return result;
+		},
+
+		resetSvgCanvas: function() {
+			console.log("reset");
+			this.counter = 3;
+			this.points = [];
+			this.canvas.on('click', this.setVerticleHandler);
+			d3.selectAll('svg > *').remove();
 		}
 	};
 
