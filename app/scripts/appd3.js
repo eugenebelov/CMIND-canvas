917fa435829137e23d89d1a347f7a09d65de3ee8
Eugene <eugene.beloff@gmail.com>
2015-11-08 21:07:27 +0200

    draggin points

--- app/scripts/appd3.js
+++ app/scripts/appd3.js
@@ -8,6 +8,7 @@
 		isDraggind: false,
 		draggedObject: null,
 		points: [],
+		resizedCoords: [],
 
 		init: function() {
 			console.log("init");
@@ -24,15 +25,47 @@
 										this.isDraggind = true;
 							    }.bind(this))
 						    .on("drag", function() {
-										d3.select(this.draggedObject)
-						                .attr("cx", (d3.event.x))
-						                .attr("cy", (d3.event.y));
+										var obj = d3.select(this.draggedObject);
+										obj.attr("cx", (d3.event.x))
+												.attr("cy", (d3.event.y));
+
 										this.updateVerticle(d3.event.x, d3.event.y);
+
+										if(this.points.length > 3) this.updateRectangle(obj);
+
 							    }.bind(this))
 						    .on("dragend", function(d) {
-											this.draggedObject = null;
-											this.isDraggind = false;
+
+									this.points = this.resizedCoords;
+									this.draggedObject = null;
+									this.isDraggind = false;
+
 							    }.bind(this))
+		},
+
+		updateRectangle: function(obj) {
+			var index = Math.floor(obj.attr('data')),
+					arr = [];
+
+			arr[0] = ( (index - 1 < 0) ? this.points[3] : this.points[index - 1]);
+			arr[1] = ({ x: Math.floor(obj.attr('cx')), y: Math.floor(obj.attr('cy'))});
+			arr[2] = ( (index + 1 > this.points.length - 1) ? this.points[0] : this.points[index + 1]);
+			arr[3] = this.calculateFourthPoint(arr);
+
+			arr.forEach(function(pt, index) {
+				var id = d3.select('#verticle' + index);
+
+				id.attr('cx', Math.floor(pt.x));
+				id.attr('cy', Math.floor(pt.y));
+
+			}.bind(this))
+
+			this.drawPoly(arr)
+					.drawCircle();
+
+			this.resizedCoords = arr;
+
+			return this;
 		},
 
 		putPointToScene: function(event) {
@@ -46,11 +79,15 @@
 				}
 				else {
 					var coords = this.points.slice(),
-							newpoint = this.calculateFourthPoint();
+							newpoint = this.calculateFourthPoint(this.points);
 
 					coords.push(newpoint);
 					this.drawPoint(newpoint.x, newpoint.y)
-							.drawPoly(coords);
+							.addVerticle(newpoint.x, newpoint.y)
+							.drawPoly(coords)
+							.drawCircle();
+
+					console.log(this.points);
 				}
 			} else {
 
@@ -66,10 +103,10 @@
 		updateVerticle: function(_x, _y) {
 			var id = d3.select(this.draggedObject).attr('data');
 
-			this.points[id].x = _x;
-			this.points[id].y = _y;
+			this.points[id].x = Math.floor(_x);
+			this.points[id].y = Math.floor(_y);
 
-			console.log("update", this.convertCoordsToPoly(this.points));
+			return this;
 		},
 
 		drawPoint: function(_x, _y) {
@@ -77,7 +114,9 @@
 					.append("circle")
 					.attr('id', 'verticle' + this.counter)
 					.attr('data', this.points.length)
-					.attr("fill", "blue")
+					.attr('fill', 'rgba(128, 0, 128, 0)')
+					.attr('stroke', 'blue')
+					.attr('stroke-width', '1')
 					.attr("cx", _x)
 					.attr("cy", _y)
 					.attr("r", 11)
@@ -89,11 +128,68 @@
 		drawPoly: function(points) {
 			var coords = this.convertCoordsToPoly(points);
 
+			d3.select('#paralelogram').remove();
+
 			this.canvas
 					.append("polygon")
+					.attr('id', 'paralelogram')
+					.attr('fill', 'rgba(128, 0, 128, 0)')
 					.attr('stroke', 'blue')
 					.attr('stroke-width', '2')
 					.attr("points", coords);
+
+			return this;
+		},
+
+		drawCircle: function() {
+			var r = this.areaRect().r,
+					center = this.areaRect().center;
+
+			d3.select('#circleOutside').remove();
+
+			this.canvas
+					.append("circle")
+					.attr('id', 'circleOutside')
+					.attr('fill', 'rgba(128, 0, 128, 0)')
+					.attr('stroke', 'blue')
+					.attr('stroke-width', '1')
+					.attr("cx", center.x)
+					.attr("cy", center.y)
+					.attr("r", r);
+
+			return this;
+		},
+
+		areaRect: function() {
+			var a = Math.floor(Math.sqrt( Math.pow(this.points[1].x - this.points[0].x, 2) + Math.pow(this.points[1].y - this.points[0].y, 2) )),
+					b = Math.floor(Math.sqrt( Math.pow(this.points[2].x - this.points[1].x, 2) + Math.pow(this.points[2].y - this.points[1].y, 2) )),
+					c = Math.floor(Math.sqrt( Math.pow(this.points[2].x - this.points[0].x, 2) + Math.pow(this.points[2].y - this.points[0].y, 2) )),
+					p = (a + b + c) /2,
+					area = Math.floor( Math.sqrt( p*(p - a)*(p - b)*(p - c))) *2;
+
+			var cx, cy;
+			var x1 = this.points[2].x,
+					x2 = this.points[0].x,
+					y1 = this.points[2].y,
+					y2 = this.points[0].y;
+
+			if( x2 > x1) cx = x1 + (x2 - x1) /2
+			if( x2 < x1) cx = x2 + (x1 - x2) /2
+			if( x2 == x1) cx = x2
+
+			if( y2 > y1) cy = y1 + (y2 - y1) /2
+			if( y2 < y1) cy = y2 + (y1 - y2) /2
+			if( y2 == y1) cy = y2
+
+			var circleRadius = Math.floor( Math.sqrt( area / Math.PI ));
+
+			return {
+				r: circleRadius,
+				center: {
+					x: cx,
+					y: cy
+				}
+			};
 		},
 
 		convertCoordsToPoly: function(coords) {
@@ -107,11 +203,12 @@
 			return result;
 		},
 
-		calculateFourthPoint: function() {
-			var _x1 = (this.points[0].x + this.points[2].x) / 2,
-					_y1 = (this.points[0].y + this.points[2].y) / 2,
-					_x2 = _x1*2 - this.points[1].x,
-					_y2 = _y1*2 - this.points[1].y;
+		calculateFourthPoint: function(points) {
+
+			var _x1 = (points[0].x + points[2].x) / 2,
+					_y1 = (points[0].y + points[2].y) / 2,
+					_x2 = _x1*2 - points[1].x,
+					_y2 = _y1*2 - points[1].y;
 
 			return {
 				x: _x2,
