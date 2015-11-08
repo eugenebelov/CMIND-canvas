d8289c0f9a672ccef75ae5411a6e5fe1caf1ccad
Eugene <eugene.beloff@gmail.com>
2015-11-08 16:06:29 +0200

    draggin points

--- app/scripts/appd3.js
+++ app/scripts/appd3.js
@@ -5,6 +5,8 @@
 		canvasNode: d3.select('#mainCanvas')[0][0],
 		drag: d3.behavior.drag(),
 		counter: 3,
+		isDraggind: false,
+		draggedObject: null,
 		points: [],
 
 		init: function() {
@@ -12,40 +14,76 @@
 			this.setVerticleHandler = this.putPointToScene.bind(this);
 			this.setResetHandler = this.resetSvgCanvas.bind(this);
 
-			this.canvas.on('click', this.setVerticleHandler);
+			this.canvas.on('mousedown', this.setVerticleHandler);
 			this.resetButton.on('click', this.setResetHandler);
+
+			var that = this;
+			this.drag.on("dragstart", function() {
+										d3.event.sourceEvent.stopPropagation();
+										this.draggedObject = d3.selectAll(d3.event.sourceEvent.target)[0];
+										this.isDraggind = true;
+							    }.bind(this))
+						    .on("drag", function() {
+										d3.select(this.draggedObject)
+						                .attr("cx", (d3.event.x))
+						                .attr("cy", (d3.event.y));
+										this.updateVerticle(d3.event.x, d3.event.y);
+							    }.bind(this))
+						    .on("dragend", function(d) {
+											this.draggedObject = null;
+											this.isDraggind = false;
+							    }.bind(this))
 		},
 
 		putPointToScene: function(event) {
 			if(d3.event.target == this.canvasNode) {
-				if( this.counter > 0) {
-					this.counter--;
-					this.points.push({ x: d3.event.offsetX, y: d3.event.offsetY });
+				if(this.counter > 0) {
 
-					this.drawPoint(d3.event.offsetX, d3.event.offsetY);
+					this.drawPoint(d3.event.offsetX, d3.event.offsetY)
+							.addVerticle(d3.event.offsetX, d3.event.offsetY);
+
+					this.counter--;
 				}
 				else {
-					this.canvas.on('click', null);
-
 					var coords = this.points.slice(),
 							newpoint = this.calculateFourthPoint();
+
 					coords.push(newpoint);
-
-					// console.log('lolol', this.convertCoordsToPoly(coords), this.points);
-
 					this.drawPoint(newpoint.x, newpoint.y)
-					this.drawPoly(coords);
+							.drawPoly(coords);
 				}
+			} else {
+
 			}
+		},
+
+		addVerticle: function(_x, _y) {
+			this.points.push({ x: _x, y: _y });
+
+			return this;
+		},
+
+		updateVerticle: function(_x, _y) {
+			var id = d3.select(this.draggedObject).attr('data');
+
+			this.points[id].x = _x;
+			this.points[id].y = _y;
+
+			console.log("update", this.convertCoordsToPoly(this.points));
 		},
 
 		drawPoint: function(_x, _y) {
 			this.canvas
 					.append("circle")
+					.attr('id', 'verticle' + this.counter)
+					.attr('data', this.points.length)
 					.attr("fill", "blue")
 					.attr("cx", _x)
 					.attr("cy", _y)
-					.attr("r", 11);
+					.attr("r", 11)
+					.call(this.drag);
+
+			return this;
 		},
 
 		drawPoly: function(points) {
